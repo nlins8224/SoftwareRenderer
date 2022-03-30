c108e408d77f672faf71d50dbfa543fa99a925c0
krystian <nlins8224@gmail.com>
2022-03-30 20:41:10 +0200

    Add NormalMappingShader

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "Vec.h"
 #include "Matrix.h"
 #include "gl.h"
-#include "TexturedShader.h"
+#include "NormalMappingShader.h"
 
 const int WIDTH = 800;
 const int HEIGHT = 800;
@@ -39,7 +39,9 @@ void render(Model& model, int width, int height, int depth) {
     // Matrix transformed = (Viewport * Projection * ModelView);
     // std::cerr << transformed << std::endl;
 
-    TexturedShader shader{ model, lightDir };
+    NormalMappingShader shader{ model, lightDir };
+    shader.set_uniform_PMV(Projection * ModelView);
+    shader.set_uniform_PMVIT((Projection * ModelView).invert_transpose());
 	for (int i = 0; i < model.nfaces(); i++) {
         Vec4f screenCoords[3];
 
